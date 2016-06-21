18ab08f8addbcd8127ce18469ab3280352d4cef8
schirrel <saneschio@gmail.com>
2016-06-21 09:13:00 -0400

    arrumando bugs

--- grafico/js/life.js
+++ grafico/js/life.js
@@ -2,13 +2,11 @@ var regions = {
         "PUB": "Public",
         "PRIV": "Private"
     },
-    w = 900,
-    h = 550,
+    w = 980,
+    h = 600,
     margin = 80,
     startYear = 2009,
     endYear = 2014,
-    startAge = 619,
-    endAge = 3286540,
     years = d3.range(startYear, endYear);
 var MIN = Number.MAX_VALUE,
     MAX = 0;
@@ -20,6 +18,9 @@ var countries_regions = {};
 var startEnd = {},
     countryCodes = {};
 
+/*
+* Encontrar os valores minimos e maximos da seleção de cursos para ajustar a escala do grafico
+*/
 function findMinMax(list) {
     list.forEach(function(l) {
         for (var i = 1; i < 6; i++) {
@@ -40,62 +41,9 @@ function findMinMax(list) {
 
 
 
-
-function OnChange() {
-    $('#filters a').toggleClass("PUB", false);
-    $('#filters a').toggleClass("PRIV", false);
-    MIN = Number.MAX_VALUE,
-        MAX = 0;
-    var e = document.getElementById("CourseSelect");
-    var selected = e.options[e.selectedIndex].text;
-    //  if(selected!="Choose Couse"){
-
-    selecionadas = [];
-
-    d3.text('linha.csv', 'text/csv', function(text) {
-        var materias = d3.csv.parseRows(text);
-        var s = ["Educação", "Direito", "Arquitetura"];
-
-        var e = document.getElementById("CourseSelect");
-        var selected = e.options[e.selectedIndex].text;
-
-        materias.forEach(function(m) {
-            //console.log(m);
-            if (m[0] == selected)
-                selecionadas.push(m);
-        });
-        // console.log(selecionadas)
-        findMinMax(selecionadas);
-        // console.log(MIN+" "+MAX)
-        // y = d3.scale.linear().domain([MAX, MIN]).range([margin , h -20]);
-        // x = d3.scale.linear().domain([2009, 2014]).range([0 + margin - 10, w-90]);
-        y = d3.scale.linear().domain([MAX, MIN - (MAX * .1)]).range([0, h]);
-        x = d3.scale.linear().domain([2009, 2014]).range([0 + margin - 10, w - 90]);
-        Chart();
-
-
-    });
-
-
-
-
-    // }
-
-}
-
-function containsCurso(a, obj) {
-
-    for (var i = 0; i < a.length; i++) {
-
-        if (a[i][0] === obj) {
-            return a[i];
-        }
-    }
-    return null;
-}
-
-
-
+/*
+* Cria o grafico para mostrar
+*/
 function Chart() {
     
     if(selecionadas.length > 0){
@@ -138,8 +86,9 @@ function Chart() {
         }
     }
 
-    vis.append("svg:line").attr("x1", x(2009)).attr("y1", y(startAge)).attr("x2", x(2014)).attr("y2", y(startAge)).attr("class", "axis").style("stroke", function(d) { return "#000"; });
-    vis.append("svg:line").attr("x1", x(startYear)).attr("y1", y(startAge)).attr("x2", x(startYear)).attr("y2", y(endAge)).attr("class", "axis").style("stroke", function(d) { return "#000"; });
+//    vis.append("svg:line").attr("x1", x(startYear)).attr("y1", y(MIN)).attr("x2", x(endYear)).attr("y2", y(MIN)).attr("class", "axis").style("stroke", function(d) { return "#000"; });
+        
+    vis.append("svg:line").attr("x1", x(startYear)).attr("y1", y(MIN)).attr("x2", x(startYear)).attr("y2", y(MAX)).attr("class", "axis").style("stroke", function(d) { return "#000"; });
     
         vis.append("svg:text")
             .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
@@ -148,40 +97,43 @@ function Chart() {
 
         vis.append("svg:text")
             .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-            .attr("transform", "translate("+ (w/2) +","+(h)+")")  // centre below axis
+            .attr("transform", "translate("+ (w/2) +","+(h-50)+")")  // centre below axis
             .text("Years");
         
-    vis.selectAll(".xLabel").data(x.ticks(5)).enter().append("svg:text").attr("class", "xLabel").text(String).attr("x", function(d) {
+    vis.selectAll(".xLabel").data(x.ticks(4)).enter().append("svg:text").attr("class", "xLabel").text(String).attr("x", function(d) {
         return x(d)
     }).attr("y", h - 10).attr("text-anchor", "middle");
     vis.selectAll(".yLabel").data(y.ticks(4)).enter().append("svg:text").attr("class", "yLabel").text(String).attr("x", 0).attr("y", function(d) {
         return y(d)
     }).attr("text-anchor", "right").attr("dy", 3);
   
-    vis.selectAll(".xTicks").data(x.ticks(3)).enter().append("svg:line").attr("class", "xTicks").attr("x1", function(d) {
+    vis.selectAll(".xTicks").data(x.ticks(6)).enter().append("svg:line").attr("class", "xTicks").attr("x1", function(d) {
         return x(d);
-    }).attr("y1", y(startAge)).attr("x2", function(d) {
+    }).attr("y1", y(MIN)).attr("x2", function(d) {
         return x(d);
-    }).attr("y2", y(startAge) + 7);
+    }).attr("y2", y(MIN) + 7);
     vis.selectAll(".yTicks").data(y.ticks(4)).enter().append("svg:line").attr("class", "yTicks").attr("y1", function(d) {
-        return y(d);
+        return y(d+60);
     }) .style("stroke", function(d) { return "#666666"; }).attr("x1", x(2009)).attr("y2", function(d) {
-        return y(d);
-    }).attr("x2", x(2014));
+        return y(d+60);
+    }).attr("x2", x(2013));
 
         
     function onclick(d, i) {
-        MakeTable();
-        $("#datatable").show();
+       
+        
 
         var currClass = d3.select(this).attr("class");
+        
         if (d3.select(this).classed('selected')) {
             d3.select(this).attr("class", currClass.substring(0, currClass.length - 9));
         } else {
             d3.select(this).classed('selected', true);
         }
     }
-
+/*
+* Muda a cor da linha e coloca o nome do curso em evidencia
+*/
     function onmouseover(d, i) {
         var currClass = d3.select(this).attr("class");
         d3.select(this).attr("class", currClass + " current");
@@ -201,104 +153,11 @@ function Chart() {
    }
 }
 
-function MakeTable(){
-var string;
-    for(var i = 1; i < 6; i++){
-        console.log(i);
-        string = '<tr class="child">';
-        for(var j = 0; i < selecionadas.length; j++){
-            console.log("\t "+j);
-            string+='<td>'+selecionadas[j]+'</td>';
-        }
-            string+='</tr>';
-         $('#datatable tbody').append(string);
-            string ='';
-    }
-  
-
-  //  $('#datatable tbody').append('<tr class="child"><td>blahblah</td></tr>');
-    
-}
-function PlotChart() {
-
-    d3.text('linha.csv', 'text/csv', function(text) {
-        var countries = d3.csv.parseRows(text);
-        for (i = 0; i < countries.length; i++) {
-            var values = countries[i].slice(1, countries[i.length - 1]);
-            var currData = [];
-
-            //countryCodes[countries[i][7]] = countries[i][0];
-            var started = false;
-
-            for (j = 0; j < values.length; j++) {
-                if (values[j] != '') {
-                    currData.push({
-                        x: years[j],
-                        y: values[j]
-                    });
-                    if (!started) {
-                        startEnd[countries[i][1]] = {
-                            'startYear': years[j],
-                            'startVal': values[j]
-                        };
-                        started = true;
-                    } else if (j == values.length - 1) {
-                        startEnd[countries[i][1]]['endYear'] = years[j];
-                        startEnd[countries[i][1]]['endVal'] = values[j];
-                    }
-                }
-            }
-            vis.append("svg:path").data([currData]).attr("diciplina", countries[i][0]).attr("class", countries[i][6]).attr("d", line).on("mouseover", onmouseover).on("mouseout", onmouseout);
-        }
-    });
-    vis.append("svg:line").attr("x1", x(2009)).attr("y1", y(startAge)).attr("x2", x(2014)).attr("y2", y(startAge)).attr("class", "axis").style("stroke", function(d) { return "#000"; })
-    vis.append("svg:line").attr("x1", x(startYear)).attr("y1", y(startAge)).attr("x2", x(startYear)).attr("y2", y(endAge)).attr("class", "axis").style("stroke", function(d) { return "#000"; })
-    vis.selectAll(".xLabel").data(x.ticks(5)).enter().append("svg:text").attr("class", "xLabel").text(String).attr("x", function(d) {
-        return x(d)
-    }).attr("y", h - 10).attr("text-anchor", "middle")
-    vis.selectAll(".yLabel").data(y.ticks(4)).enter().append("svg:text").attr("class", "yLabel").text(String).attr("x", 0).attr("y", function(d) {
-        return y(d)
-    }).attr("text-anchor", "right").attr("dy", 3)
-    vis.selectAll(".xTicks").data(x.ticks(3)).enter().append("svg:line").attr("class", "xTicks").attr("x1", function(d) {
-        return x(d);
-    }).attr("y1", y(startAge)).attr("x2", function(d) {
-        return x(d);
-    }).attr("y2", y(startAge) + 7)
-    vis.selectAll(".yTicks").data(y.ticks(4)).enter().append("svg:line").attr("class", "yTicks").attr("y1", function(d) {
-        return y(d);
-    }).attr("x1", x(2009)).attr("y2", function(d) {
-        return y(d);
-    }).attr("x2", x(2014))
-
-    function onclick(d, i) {
-        var currClass = d3.select(this).attr("class");
-        if (d3.select(this).classed('selected')) {
-            d3.select(this).attr("class", currClass.substring(0, currClass.length - 9));
-        } else {
-            d3.select(this).classed('selected', true);
-        }
-    }
-
-    function onmouseover(d, i) {
-        var currClass = d3.select(this).attr("class");
-        d3.select(this).attr("class", currClass + " current");
-        var countryCode = $(this).attr("diciplina");
-        //console.log(countryCode);
-        var countryVals = startEnd[countryCode];
-        //  var percentChange = 100 * (countryVals['endVal'] - countryVals['startVal']) / countryVals['startVal'];
-        var blurb = countryCode;
 
 
-        //        blurb += "</p>";
-
-        $("#blurb-content").html(blurb);
-    }
-
-
-
-
-}
-
+/*
+* Volta a cor da linha ao normal
+*/
 function onmouseout(d, i) {
     var currClass = d3.select(this).attr("class");
     var prevClass = currClass.substring(0, currClass.length - 8);
@@ -307,13 +166,18 @@ function onmouseout(d, i) {
 //        $("#blurb-content").html('<br />');
 }
 
-function showRegion(regionCode) {
-    var countries = d3.selectAll("path." + regionCode);
 
-    if (countries.classed('highlight')) {
-        countries.attr("class", regionCode);
+/*
+* Mudar a cor da linha do curso quando selecionado o tipo de universidade
+*/
+
+function showCourse(courseCode) {
+    var courses = d3.selectAll("path." + courseCode);
+
+    if (courses.classed('highlight')) {
+        courses.attr("class", courseCode);
     } else {
-        countries.classed('highlight', true);
+        courses.classed('highlight', true);
     }
 }
 
@@ -321,23 +185,24 @@ function showRegion(regionCode) {
 
 function Load() {
 
-    d3.text('linhas-pub-priv.csv', 'text/csv', function(text) {
+d3.text('linhas-pub-priv.csv', 'text/csv', function(text) {
         var materias = d3.csv.parseRows(text);
-        var cursos = [];
-        var select = $("#CourseSelect");
        
-        for (var i = 1; i < materias.length / 2; i++) {
-            select.append($('<option>', {
-    text: materias[i][0]
-}));
-        }
-
-    });
-
-    ChartInit();
+  materias.forEach(function(m) {
+            selecionadas.push(m);
+        });
+    
+MakeCheck();
+     ChartInit();
+});
+   
   
 }
 
+
+/*
+* Cria os checkbox de acordo com o arquivo de cursos
+*/
 function MakeCheck(){
 
     var container = document.getElementById("CourseChecks");
@@ -370,6 +235,10 @@ container.appendChild(label);
     };
 
 }
+
+/*
+*Atualiza grafico quando selecionado checkbox
+*/
 function Check(){
     var checkeds = [];
 $("input:checkbox[name=courseCheck]:checked").each(function(){
@@ -377,34 +246,25 @@ $("input:checkbox[name=courseCheck]:checked").each(function(){
 });
     
  
-       $('#filters a').toggleClass("PUB", false);
+    $('#filters a').toggleClass("PUB", false);
     $('#filters a').toggleClass("PRIV", false);
     MIN = Number.MAX_VALUE,
         MAX = 0;
     selecionadas = [];
 
-    d3.text('linha.csv', 'text/csv', function(text) {
+    d3.text('linhas-pub-priv.csv', 'text/csv', function(text) {
         var materias = d3.csv.parseRows(text);
 
         checkeds.forEach(function(c) {
-//           aux = containsCurso(materias, m);
-//            console.log(aux);
-//            if (aux!=null){
-//
-//                selecionadas.push(aux); }
              materias.forEach(function(m) {
-            //console.log(m);
             if (m[0] == c)
                 selecionadas.push(m);
         });
         });
        
         findMinMax(selecionadas);
-      //  console.log(MIN+" "+MAX);
-        // y = d3.scale.linear().domain([MAX, MIN]).range([margin , h -20]);
-        // x = d3.scale.linear().domain([2009, 2014]).range([0 + margin - 10, w-90]);
-        y = d3.scale.linear().domain([MAX, MIN - (MAX * .1)]).range([0, h]);
-        x = d3.scale.linear().domain([2009, 2014]).range([0 + margin - 10, w - 90]);
+   y = d3.scale.linear().domain([MAX, MIN - (MAX * .2)]).range([0 + margin, h]);
+        x = d3.scale.linear().domain([2009, 2013.5]).range([0 + margin - 10, w - 10]);
         Chart();
 
 
@@ -412,23 +272,12 @@ $("input:checkbox[name=courseCheck]:checked").each(function(){
     
     
 }
-function ChartInit() {
-    d3.text('linha.csv', 'text/csv', function(text) {
-        var materias = d3.csv.parseRows(text);
-       
-console.log($("#CourseSelect").selected);
-        //var e = document.getElementById("CourseSelect");
-       //var selected = e.options[e.selectedIndex].text;
 
-        materias.forEach(function(m) {
-            selecionadas.push(m);
-        });
-  
-        findMinMax(selecionadas);
+
+function ChartInit() {
+      findMinMax(selecionadas);
         y = d3.scale.linear().domain([MAX, MIN - (MAX * .2)]).range([0 + margin, h]);
-        x = d3.scale.linear().domain([2009, 2014]).range([0 + margin - 10, w - 10]);
-        Chart();
-MakeCheck();
-    })
-    
+        x = d3.scale.linear().domain([2009, 2013.5]).range([0 + margin - 10, w - 10]);
+            Chart();
+
 }
