8f0c4f734bedcca3d7ab6e2de24b885f07621857
schirrel <saneschio@gmail.com>
2016-06-21 09:57:32 -0400

    rrefat

--- grafico/js/main.js
+++ grafico/js/main.js
@@ -1,250 +1,280 @@
-    'use strict';
-    var color = d3.scale.category10();
-    var parseDate = d3.time.format("%Y").parse;
-    var ObjsId = [];
-    var dataNames = [];
-    function AddCourse() {
-        var e = document.getElementById("CourseSelect");
-        var selected = e.options[e.selectedIndex].text;
-        $('#TagsCourses').tagsinput('add', selected);
-    }
+var regions = {
+        "PUB": "Public",
+        "PRIV": "Private"
+    },
+    w = 980,
+    h = 600,
+    margin = 80,
+    startYear = 2009,
+    endYear = 2014,
+    years = d3.range(startYear, endYear);
+var MIN = Number.MAX_VALUE,
+    MAX = 0;
+var selecionadas = [];
+var color = d3.scale.category10();
 
+var countries_regions = {};
 
-    function Load(){
-    d3.tsv("novo.csv", function(error, data) {
-      if (error) throw error;
+var startEnd = {},
+    countryCodes = {};
 
-      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+/*
+ * Encontrar os valores minimos e maximos da seleção de cursos para ajustar a escala do grafico
+ */
+function findMinMax(list) {
+    list.forEach(function(l) {
+        for (var i = 1; i < 6; i++) {
 
-      data.forEach(function(d) {
-
-       d.date = parseDate(d.date);
-      });
-
-
-      var materias = color.domain().map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {name: +d[name], date: d.date};
-          })
-        };
-      });
-        var x = document.getElementById("CourseSelect");
-
-    materias.forEach(function(m){
-           // console.log();
-        var option = document.createElement("option");
-    option.text = m.name;
-    x.add(option);
-        });   
-
-
-    }
-           );
-           }
-
-    function contains(a, obj) {
-        for (var i = 0; i < a.length; i++) {
-           // console.log(a[i]+ " "+obj)
-            if (a[i] === obj) { 
-                return true;
+            if (Number(l[i]) < MIN) {
+                MIN = Number(l[i]);
+            }
+            if (Number(l[i]) > MAX) {
+                MAX = Number(l[i]);
             }
         }
-        return false;
-    }
+    });
+}
 
-    function containsCurso(a, obj) {
-      //  console.log(obj);
-        for (var i = 0; i < a.length; i++) {
-            if (a[i].Curso === obj) { 
-                return a[i];
+
+
+/*
+ * Cria o grafico para mostrar
+ */
+function Chart() {
+
+    if (selecionadas.length > 0) {
+        document.getElementById("vis").innerHTML = "";
+        var vis = d3.select("#vis").append("svg:svg").attr("width", w).attr("height", h).append("svg:g");
+        var line = d3.svg.line().x(function(d, i) {
+            return x(d.x);
+        }).y(function(d) {
+            return y(d.y);
+        });
+        
+        for (i = 0; i < selecionadas.length; i++) {
+            var values = selecionadas[i].slice(1, selecionadas[i.length - 1]);
+            var currData = [];
+
+            //countryCodes[countries[i][7]] = countries[i][0];
+            var started = false;
+
+            for (j = 0; j < values.length; j++) {
+                if (values[j] != '') {
+                    currData.push({
+                        x: years[j],
+                        y: values[j]
+                    });
+                    if (!started) {
+                        startEnd[selecionadas[i][1]] = {
+                            'startYear': years[j],
+                            'startVal': values[j]
+                        };
+                        started = true;
+                    } else if (j == values.length - 1) {
+                        startEnd[selecionadas[i][1]]['endYear'] = years[j];
+                        startEnd[selecionadas[i][1]]['endVal'] = values[j];
+                    }
+                }
+            }
+            vis.append("svg:path").data([currData]).attr("diciplina", selecionadas[i][0]).attr("class", selecionadas[i][6]).attr("d", line).on("mouseover", onmouseover).on("mouseout", onmouseout);
+            if (selecionadas.length < 3) {
+                vis.on("click", onclick);
             }
         }
-        return null;
-    }
 
+            vis.append("svg:line").attr("x1", x(startYear)).attr("y1", y(MIN)).attr("x2", x(2013.5)).attr("y2", y(MIN)).attr("class", "axis").style("stroke", function(d) { return "#000"; });
 
-    function OpenData() {
-        d3.tsv("cursosId.csv", function(error, data) {
-      if (error) throw error;
-
-      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-
-      data.forEach(function(d) {
-
-          ObjsId.push(d);
-      });
-                   var materias=[]
-        var tags = $("#TagsCourses").tagsinput('items');
-
-        tags.forEach(function(m){
-     var aux = containsCurso(ObjsId, m);
-            if(aux!=null)
-                materias.push(aux);
+        vis.append("svg:line").attr("x1", x(startYear)).attr("y1", y(MIN)).attr("x2", x(startYear)).attr("y2", y(MAX)).attr("class", "axis").style("stroke", function(d) {
+            return "#000";
         });
 
-    //     console.log(tags);  
-    // console.log(materias);  
-    var check = $('input[name="optionsRadios"]:checked', '#myForm').val();
+        vis.append("svg:text")
+            .attr("text-anchor", "middle") // this makes it easy to centre the text as the transform is applied to the anchor
+            .attr("transform", "translate(" + 60 + "," + (h / 2) + ")rotate(-90)") // text is drawn off the screen top left, move down and out and rotate
+            .text("Number of Students (square root)");
 
-        if(check != "all"){
+        vis.append("svg:text")
+            .attr("text-anchor", "middle") // this makes it easy to centre the text as the transform is applied to the anchor
+            .attr("transform", "translate(" + (w / 2) + "," + (h - 50) + ")") // centre below axis
+            .text("Years");
 
-        materias.forEach(function(m){
-            dataNames.push("data-"+check+"-"+m.ID+".csv");
-        });
+        vis.selectAll(".xLabel").data(x.ticks(4)).enter().append("svg:text").attr("class", "xLabel").text(String).attr("x", function(d) {
+            return x(d)
+        }).attr("y", h - 10).attr("text-anchor", "middle");
+        vis.selectAll(".yLabel").data(y.ticks(4)).enter().append("svg:text").attr("class", "yLabel").text(String).attr("x", 0).attr("y", function(d) {
+            return y(d)
+        }).attr("text-anchor", "right").attr("dy", 3);
+
+        vis.selectAll(".xTicks").data(x.ticks(6)).enter().append("svg:line").attr("class", "xTicks").attr("x1", function(d) {
+            return x(d);
+        }).attr("y1", y(MIN)).attr("x2", function(d) {
+            return x(d);
+        }).attr("y2", y(MIN) + 7);
+        vis.selectAll(".yTicks").data(y.ticks(4)).enter().append("svg:line").attr("class", "yTicks").attr("y1", function(d) {
+            return y(d + 60);
+        }).style("stroke", function(d) {
+            return "#666666";
+        }).attr("x1", x(2009)).attr("y2", function(d) {
+            return y(d + 60);
+        }).attr("x2", x(2013));
+
+
+        function onclick(d, i) {
+
+
+
+            var currClass = d3.select(this).attr("class");
+
+            if (d3.select(this).classed('selected')) {
+                d3.select(this).attr("class", currClass.substring(0, currClass.length - 9));
+            } else {
+                d3.select(this).classed('selected', true);
+            }
         }
-            MakeData();
+        /*
+         * Muda a cor da linha e coloca o nome do curso em evidencia
+         */
+        function onmouseover(d, i) {
+            var currClass = d3.select(this).attr("class");
+            d3.select(this).attr("class", currClass + " current");
+            var countryCode = $(this).attr("diciplina");
+            //console.log(countryCode);
+            var countryVals = startEnd[countryCode];
+            //  var percentChange = 100 * (countryVals['endVal'] - countryVals['startVal']) / countryVals['startVal'];
+            var blurb = countryCode;
+
+
+            //        blurb += "</p>";
+
+            $("#blurb-content").html(blurb);
+        }
+    } else {
+        Load();
+    }
+    
+   
+   
+}
+
+
+
+/*
+ * Volta a cor da linha ao normal
+ */
+function onmouseout(d, i) {
+    var currClass = d3.select(this).attr("class");
+    var prevClass = currClass.substring(0, currClass.length - 8);
+    d3.select(this).attr("class", prevClass);
+    //        $("#default-blurb").show();
+    //        $("#blurb-content").html('<br />');
+}
+
+
+/*
+ * Mudar a cor da linha do curso quando selecionado o tipo de universidade
+ */
+
+function showCourse(courseCode) {
+    var courses = d3.selectAll("path." + courseCode);
+
+    if (courses.classed('highlight')) {
+        courses.attr("class", courseCode);
+    } else {
+        courses.classed('highlight', true);
+    }
+}
+
+
+/*
+ * Quando a pagina é totalmente carregada, inicia o grafico com todos os valores e chama MakeCheck  
+ */
+function Load() {
+
+    d3.text('linhas-pub-priv.csv', 'text/csv', function(text) {
+        var materias = d3.csv.parseRows(text);
+
+        materias.forEach(function(m) {
+            selecionadas.push(m);
+        });
+
+        MakeCheck();
+        findMinMax(selecionadas);
+        y = d3.scale.linear().domain([MAX, MIN - (MAX * .2)]).range([0 + margin, h]);
+        x = d3.scale.linear().domain([2009, 2013.5]).range([0 + margin - 10, w - 10]);
+        Chart();
     });
 
 
+}
+
+
+/*
+ * Cria os checkbox de acordo com o arquivo de cursos
+ */
+function MakeCheck() {
+
+    var container = document.getElementById("CourseChecks");
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
 
-    function MakeData(){ 
-    dataNames.forEach(function(names){
-       console.log(names); 
+
+    for (var j = 1; j < selecionadas.length / 2; j++) {
+
+        var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.name = "courseCheck";
+        checkbox.value = selecionadas[j][0];
+        checkbox.id = "courseCheck";
+        checkbox.style.marginLeft = "5px";
+        checkbox.style.marginRight = "5px";
+        checkbox.onclick = function() {
+            Check();
+        }
+        var label = document.createElement('label')
+        label.htmlFor = "id";
+        label.appendChild(document.createTextNode(selecionadas[j][0]));
+
+        container.appendChild(checkbox);
+        container.appendChild(label);
+        if (j > 0 && j % 2 == 0)
+            container.appendChild(document.createElement("br"));
+    };
+
+}
+
+/*
+ *Atualiza grafico quando selecionado checkbox
+ */
+function Check() {
+    var checkeds = [];
+    $("input:checkbox[name=courseCheck]:checked").each(function() {
+        checkeds.push($(this).val());
+    });
 
 
-            d3.tsv("data/"+names, function(error, data) {
-      if (error) throw error;
-console.log(data);
+    $('#filters a').toggleClass("PUB", false);
+    $('#filters a').toggleClass("PRIV", false);
+    MIN = Number.MAX_VALUE,
+        MAX = 0;
+    selecionadas = [];
+
+    d3.text('linhas-pub-priv.csv', 'text/csv', function(text) {
+        var materias = d3.csv.parseRows(text);
+
+        checkeds.forEach(function(c) {
+            materias.forEach(function(m) {
+                if (m[0] == c)
+                    selecionadas.push(m);
             });
         });
-    }
 
-    function ShowData(){
-
-    }
-    function ShowChart() {
-        var color = d3.scale.category10();
-    document.getElementById("legenda").style.display =  "block";
-   
-        document.getElementById("grafico").style.display =  "block";
-        document.getElementById("grafico").innerHTML = "";
-         document.getElementById("leg").innerHTML = "";
-        var margin = {top: 20, right: 0, bottom: 30, left: 50},
-        width = 760 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
-    var parseDate = d3.time.format("%Y").parse;
-
-    var x = d3.time.scale()
-        .range([0, width]);
-
-
-
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-
-       xAxis.ticks(d3.time.years);
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.name); });
-
-    var svg = d3.select("#grafico").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.tsv("novo.csv", function(error, data) {
-      if (error) throw error;
-
-      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
-
-      data.forEach(function(d) {
-       d.date = parseDate(d.date);
-      });
-
-
-      var aux = color.domain().map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {name: +d[name], date: d.date};
-          })
-        };
-      });
-
-        var materias=[]
-        var tags = $("#TagsCourses").tagsinput('items');
-
-
-        aux.forEach(function(m){
-            if(contains(tags, m.name)){
-                materias.push(m);
-            }
-        });
-      x.domain(d3.extent(data, function(d) { return d.date; }));
-
-      y.domain([
-        d3.min(materias, function(c) { return d3.min(c.values, function(v) { return v.name; }); }),
-        d3.max(materias, function(c) { return d3.max(c.values, function(v) { return v.name; }); })
-      ]);
-
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end");
-    //      .text("Matricula");
-    var DivLegendas = document.getElementById("leg");
-      var materia = svg.selectAll(".materia")
-          .data(materias)
-        .enter().append("g")
-          .attr("class", "materia");
-      materia.append("path")
-          .attr("class", "line")
-          .attr("d", function(d) { return line(d.values); })
-          .style("stroke", function(d) {
-          var tempElement = document.createElement('div');
-    tempElement.innerHTML = "<div style='color:"+color(d.name)+"'>"+d.name+"</div>";
-    document.getElementById("leg").appendChild(tempElement);
-      return color(d.name); 
-
-      });
-    //
-    //  materia.append("text")
-    //      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-    //      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.name) + ")"; })
-    //      .attr("x", 3)
-    //      .attr("dy", ".35em")
-    //      .text(function(d) { return d.name; });
-    //materia.forEach(function(m){
-    //        
-    //        console.log(m.name+ " " +color(m.name));
-    //           
-    //        
-    //    });
-
+        findMinMax(selecionadas);
+        console.log(MIN+" "+MAX);
+        y = d3.scale.linear().domain([MAX, MIN - (MAX * .2)]).range([0 + margin, h]);
+        x = d3.scale.linear().domain([2009, 2013.5]).range([0 + margin - 10, w - 10]);
+        Chart();
     });
 
-
-    }
-
-    function NewChart() {
-        document.getElementById("legenda").style.display =  "none";
-        document.getElementById("fNovo").style.display =  "none";
-        document.getElementById("fInit").style.display =  "block";
-         document.getElementById("grafico").style.display =  "none";
-    }
+}
