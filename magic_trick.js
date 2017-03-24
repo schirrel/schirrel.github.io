fef8681c50b3e1e843b07a9076f473040d78ee9e
CoderSquirrel <saneschio@gmail.com>
2017-03-24 03:44:33 -0400

    page

--- magic_trick.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-$("li.custom_nav" ).click(function() {
-$('.site_content').find("*").removeClass("active");
-
-$("#"+$(this).data("target")).addClass("active");
-});
-}); 
