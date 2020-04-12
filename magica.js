5b2e34c6bacf7b9291549c9c2b3362ff9d4d5211
Alan Balen Schio <alanbschio@gmail.com>
2020-04-11 22:31:31 -0400

    Finalizado uma parte do menu

--- magica.js
+++ magica.js
@@ -1,4 +1,18 @@
-import initDictionary from './translation.js';
+const menuItens = Array.from(document.querySelectorAll('aside nav a'));
+
+const handleMenuClick = (event) => {
+    menuItens.forEach(item => item.className = "");
+    event.target.className = 'active';
+}
 
 
-initDictionary();
+
+// [...document.querySelectorAll('aside nav a')].forEach()
+menuItens.forEach((item) => {
+    item.onclick = handleMenuClick;
+})
+
+
+// document.addEventListener('scroll', () => {
+//     document.body.classList.toggle('scrolling')
+// })
