20a967b8cf4f253cc4fb1e2718d4ea724ca59793
Alan Schio <alanbschio@gmail.com>
2025-01-27 12:38:27 -0800

    new page

--- magica.js
+++
@@ -1,7 +0,0 @@
-import Dictionary from './scripts/translation.js';
-import Menu from './scripts/menu.js';
-import Content from './scripts/content.js';
-
-Dictionary.init();
-Content.init();
-Menu.init();
